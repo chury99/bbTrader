90ed330a1b5731437e5990508c6eac874a361984
chury99 <chury99@gmail.com>
2026-07-28 18:11:00 +0900

    안쓰는 코드 정리

--- launcher_trader.py
+++ launcher_trader.py
@@ -108,57 +108,6 @@ class LauncherTrader:
             else:
                 self.send_카톡_오류발생(s_프로세스명=p_봇.name, n_오류코드=p_봇.exitcode)
 
-    def run_트레이더(self):
-        """ 트레이딩을 위한 bot 실행 """
-        # 프로세스 생성 및 실행
-        dic_봇정보 = dict(s_타겟=trader.bot_트레이딩.run, s_네임='봉봉trader')
-        p_봇 = mp.Process(target=dic_봇정보['s_타겟'], name=dic_봇정보['s_네임'])
-        p_봇.start()
-
-        # 감시 루프 구동
-        b_동작중 = True
-        dt_에러발생 = pd.Timestamp.now()
-        while True:
-            # 종료시간 이후라면 프로세스 종료
-            if pd.Timestamp.now() > pd.Timestamp(self.s_종료시각):
-                ret = p_봇.terminate() if p_봇.is_alive() else None
-                b_동작중 = False
-
-            # 종료시간 내 비정상 종료 시 재시작
-            elif not p_봇.is_alive():
-                # 오류 발생 시 재실행
-                if p_봇.exitcode == 1:
-                    # 연속 재실행 검사
-                    if pd.Timestamp.now() - dt_에러발생 < pd.Timedelta(seconds=3):
-                        b_동작중 = False
-                        continue
-                    # 모듈 재실행
-                    self.kakao.send_메세지(s_사용자='알림봇', s_수신인='여봉이', s_메세지=f'{p_봇.name} 모듈 재시작')
-                    p_봇 = mp.Process(target=dic_봇정보['s_타겟'], name=dic_봇정보['s_네임'])
-                    p_봇.start()
-                    dt_에러발생 = pd.Timestamp.now()
-
-                # 이외 경우에는 오류 알림
-                else:
-                    self.send_카톡_오류발생(s_프로세스명=p_봇.name, n_오류코드=p_봇.exitcode)
-                    b_동작중 = False
-
-            # 동작 종료 시 감시 루프 해제
-            if not b_동작중:
-                break
-
-            # 확인 주기 설정
-            time.sleep(0.1)
-
-        # 프로세스 종료 처리
-        p_봇.join()
-
-        # 로그 기록
-        if p_봇.exitcode <= 0:
-            self.make_로그(f'{p_봇.name} 구동 완료')
-        else:
-            self.send_카톡_오류발생(s_프로세스명=p_봇.name, n_오류코드=p_봇.exitcode)
-
     def send_카톡_오류발생(self, s_프로세스명, n_오류코드):
         """ 실행 오류 발생 시 프로세스명 포함하여 카톡 메세지 송부 """
         # 메세지 정의
@@ -172,7 +121,6 @@ class LauncherTrader:
 def run():
     """ 실행 함수 """
     l = LauncherTrader()
-    # l.run_트레이더()
     l.run_실시간()
 
 
